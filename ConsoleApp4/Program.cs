a5f9958ce14a03ce46927bcd1affe3e4dd6ab560
iaguilar313 <iaguilar@umich.edu>
2017-09-29 16:30:14 -0400

    first

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool ProgramContinue = true;
+            //while (ProgramContinue ==true)
+
+            Console.WriteLine("Please Enter 1 Word");
+            //reads user input an converts it to lowercase 
+            string userInput = Console.ReadLine().ToLower();
+
+            //isolates first index of string
+            // checks if index value 0 is a vowel 
+            //string[0] tells it to look at index 0 can place any index 
+
+            if (userInput[0] == 'a' || userInput[0] == 'e' || userInput[0] == 'i' || userInput[0] == 'o' || userInput[0] == 'u')
+            {
+                Console.WriteLine(userInput + "way");
+            }
+            else
+            {
+                int pig = userInput.IndexOfAny(new char[] { 'a', 'e', 'o', 'u', 'i' });
+
+                Console.WriteLine((userInput.Substring(pig)) + (userInput.Substring(0,pig)) +"`" +
+                    "ay");
+
+
+                //userInput.IndexOf('a') != -1 || userInput.IndexOf('e') != -1 || userInput.IndexOf('i') != -1 || userInput.IndexOf('o') != -1 || userInput.IndexOf('u') != -1
+
+               // string pig = userInput.SubString();
+            }
+            
+        }
+    }
+}
