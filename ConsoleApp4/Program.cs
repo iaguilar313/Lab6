9ae8e94d7d05c5a1209f98a6e12133b20418417e
iaguilar313 <iaguilar@umich.edu>
2017-10-02 14:34:50 -0400

    update validation

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -10,34 +10,56 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-            //bool ProgramContinue = true;
-            //while (ProgramContinue ==true)
-
-            Console.WriteLine("Please Enter 1 Word");
-            //reads user input an converts it to lowercase 
-            string userInput = Console.ReadLine().ToLower();
-
-            //isolates first index of string
-            // checks if index value 0 is a vowel 
-            //string[0] tells it to look at index 0 can place any index 
-
-            if (userInput[0] == 'a' || userInput[0] == 'e' || userInput[0] == 'i' || userInput[0] == 'o' || userInput[0] == 'u')
+            bool ProgramContinue = true;
+            while (ProgramContinue == true)
             {
-                Console.WriteLine(userInput + "way");
+                Console.WriteLine("Please Enter 1 Word");
+                //reads user input an converts it to lowercase 
+                string userInput = Console.ReadLine().ToLower();
+
+                //isolates first index of string
+                // checks if index value 0 is a vowel 
+                //string[0] tells it to look at index 0 can place any index 
+
+                if (userInput[0] == 'a' || userInput[0] == 'e' || userInput[0] == 'i' || userInput[0] == 'o' || userInput[0] == 'u')
+                {
+                    Console.WriteLine(userInput + "way");
+                }
+                else
+                {
+                    int pig = userInput.IndexOfAny(new char[] { 'a', 'e', 'o', 'u', 'i' });
+
+                    Console.WriteLine((userInput.Substring(pig)) + (userInput.Substring(0, pig)) + "`" +
+                        "ay");
+
+
+                    //userInput.IndexOf('a') != -1 || userInput.IndexOf('e') != -1 || userInput.IndexOf('i') != -1 || userInput.IndexOf('o') != -1 || userInput.IndexOf('u') != -1
+
+                    // string pig = userInput.SubString();
+                }
+                String UserChoice;
+                Console.WriteLine("Do you want to roll the dice again? (No/yes)");
+                UserChoice = ReadValidtion();
+                if (UserChoice == "no")
+                {
+                    ProgramContinue = false;
+                }
+
             }
-            else
+        }
+
+        public static string ReadValidtion ()
+        {
+            Console.WriteLine("Do you want to continue? Yes or No");
+            string Input= Console.ReadLine().ToLower();
+            while (Input!=
+                "yes" && Input != "no" )
             {
-                int pig = userInput.IndexOfAny(new char[] { 'a', 'e', 'o', 'u', 'i' });
-
-                Console.WriteLine((userInput.Substring(pig)) + (userInput.Substring(0,pig)) +"`" +
-                    "ay");
-
-
-                //userInput.IndexOf('a') != -1 || userInput.IndexOf('e') != -1 || userInput.IndexOf('i') != -1 || userInput.IndexOf('o') != -1 || userInput.IndexOf('u') != -1
-
-               // string pig = userInput.SubString();
+                Console.WriteLine("Please enter either Yes or No");
+                Input = Console.ReadLine().ToLower();
             }
-            
+
+            return Input;
         }
     }
 }
